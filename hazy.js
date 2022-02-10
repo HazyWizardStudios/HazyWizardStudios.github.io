4e974ba3b5d11b6a5c2ea5f3ee7e4096241734ac
James Clark <hazywizardstudio@gmail.com>
2022-02-10 01:57:38 -0500

    1

--- hazy.js
+++ hazy.js
@@ -1,1 +1,3 @@
 function changePage(newPage){console.log(newPage);document.getElementById("bodyMain").innerHTML=`<object type="text/html" data=${newPage}></object>`;}
+
+window.onload=function(){if(document.getElementById('newsButton')!=null||document.getElementById('newsButton')!=""){document.getElementById('newsButton').click();}}
