fc21591333c7126c6031474fd4762a54eb101748
James Clark <hazywizardstudio@gmail.com>
2022-02-21 02:24:14 -0500

    aa

--- hazy.js
+++ hazy.js
@@ -1,3 +1,4 @@
 function changePage(newPage){console.log(newPage);document.getElementById("bodyMain").innerHTML=`<object type="text/html" data=${newPage}></object>`;}
+function changePagePanel(newPage){console.log(newPage);this.ParentElement.document.getElementById("bodyMain").innerHTML=`<object type="text/html" data=${newPage}></object>`;}
 
-window.onload=function(){if(document.getElementById('newsButton')!=null||document.getElementById('newsButton')!=""){document.getElementById('newsButton').click();}}
+/*window.onload=function(){if(document.getElementById('newsButton')!=null||document.getElementById('newsButton')!=""){document.getElementById('newsButton').click();}}*/
