f47152440db9974a5553a5b02160e480d03db2ea
James Clark <hazywizardstudio@gmail.com>
2022-03-29 14:07:02 -0400

    hamburger modal

--- hazy.js
+++ hazy.js
@@ -1,4 +1,14 @@
-function changePage(newPage){console.log(newPage);document.getElementById("bodyMain").innerHTML=`<object type="text/html" data=${newPage}></object>`;}
+
+const burgerMenu = document.querySelector("#hambuger")
+
+function changePage(newPage){console.log(newPage);document.getElementById("bodyMain").innerHTML=`<object type="text/html" data=pages/${newPage}></object>`;document.getElementById("hamburgerMenu").style.display="none"}
 
 window.addEventListener("load", function(){
-    changePage("News.html");})
+    changePage("News.html");
+    }
+)
+
+function toggleMenu(){
+    document.getElementById("hamburgerMenu").style.display="flex";
+    console.log("clicked")
+}
